b32ed4d79a676ca700c6a1586a7d5f20b6059453
Ngrdj <ngrdjhotmail.fr>
2020-04-20 21:07:46 +0200

    creation de l'interface de base + déclaration de variables

---
+++ script/script.js
@@ -0,0 +1,8 @@
+let todo = {
+done: false,
+libelle: 'faire la todo list',
+id:'1'
+}
+
+const toDoInput = document.getElementById("toDoInput");
+const toDoList = document.getElementById("toDoList");
