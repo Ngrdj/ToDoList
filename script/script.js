0dfdf9886ec9d0119593b573ccb55a8dbb686409
Ngrdj <ngrdjhotmail.fr>
2020-04-20 21:32:48 +0200

    Ajout de la fonction (createToDo)

--- script/script.js
+++ script/script.js
@@ -6,3 +6,21 @@ id:'1'
 
 const toDoInput = document.getElementById("toDoInput");
 const toDoList = document.getElementById("toDoList");
+
+document.addEventListener("keydown", event => {
+  if (event.keyCode === 13) {
+      createToDo();  
+  }
+});
+
+function createToDo() {
+    if(toDoInput.value === ""){
+        alert("veuillez entrer une tâche à réaliser")
+    }
+    else{
+        const item = document.createElement("li");
+        item.innerHTML = toDoInput.value;
+        
+        toDoList.appendChild(item);
+    }
+}
